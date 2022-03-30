5ff9792cbff0dad24f5daf35a02e7a475ae8d8bd
Elvis Huges <elvis.huges@blockhub.builders>
2022-03-30 01:40:48 -0300

    🎨 adicionando inputs em tela de messages

--- src/pages/Messages.vue
+++ src/pages/Messages.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="q-pa-md">
-    <q-form @submit="onSubmit" class="q-gutter-md">
+    <q-form @submit="onSubmit">
       <div class="row">
         <q-input
           v-model="form.gameTitle"
@@ -25,6 +25,95 @@
           hint="Name and surname"
           lazy-rules
         />
+      </div>
+      <div class="row">
+        <q-input
+          v-model="form.gameTitle"
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Game title"
+          hint="the game title"
+          lazy-rules
+        />
+        <q-input
+          v-model="form.initialImage"
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Initial Image"
+          hint="Name and surname"
+          lazy-rules
+        />
+        <q-input
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Your name *"
+          hint="Name and surname"
+          lazy-rules
+        />
+      </div>
+      <div class="row">
+        <q-input
+          v-model="form.gameTitle"
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Game title"
+          hint="the game title"
+          lazy-rules
+        />
+        <q-input
+          v-model="form.initialImage"
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Initial Image"
+          hint="Name and surname"
+          lazy-rules
+        />
+        <q-input
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Your name *"
+          hint="Name and surname"
+          lazy-rules
+        />
+      </div>
+      <div class="row">
+        <q-input
+          v-model="form.gameTitle"
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Game title"
+          hint="the game title"
+          lazy-rules
+        />
+        <q-input
+          v-model="form.initialImage"
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Initial Image"
+          hint="Name and surname"
+          lazy-rules
+        />
+        <q-input
+          class="col-xs-12 col-sm-4 col-md-4 q-px-sm q-pt-md"
+          outlined
+          label="Your name *"
+          hint="Name and surname"
+          lazy-rules
+        />
+      </div>
+      <div class="row justify-end q-pa-md q-gutter-sm">
+        <q-btn
+          align="around"
+          color="grey"
+          label="Back"
+          size="lg"
+          @click="$router.go(-1)"
+        />
+        <q-btn type="submit" label="Save" color="primary" size="lg">
+          <template v-slot:loading>
+            <q-spinner-facebook />
+          </template>
+        </q-btn>
       </div>
     </q-form>
   </q-page>
